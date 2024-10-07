6454f66db2c1137aaa0d665de7a225443178b463
kkanami <k3064782@gmail.com>
2024-10-07 22:34:33 +0900

    docs:debugする

--- debug.js
+++ debug.js
@@ -1,16 +1,19 @@
    function toOneDimension(previousValue, currentValue) {
-       return previousValue.concat(currentValue);
+       return currentValue.concat(previousValue);
    }
 
    var sampleArrayA = [
 ['A1', 'A2', 'A3'],
 ['B1', 'B2', 'B3'],
-['C1', 'C2', 'C3']
+['C1', 'C2', 'C3'],
+['D1', 'D2', 'D3']
 ];
 
-   sampleArrayA.reverse();
-
    var sampleArrayB = sampleArrayA.reduce(toOneDimension);
+
+
+   console.count(toOneDimension);
+   console.log(sampleArrayB);
 
 
    for (var counterVar = 0; counterVar < sampleArrayB.length; counterVar++) {
